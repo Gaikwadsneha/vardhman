b47e418311040f02cf0e9fd3c02ae6cf59b2a42b
Gaikwadsneha <51084089+Gaikwadsneha@users.noreply.github.com>
2019-06-02 14:57:09 +0530

    Add files via upload

--- nachya.js
+++ nachya.js
@@ -1,8 +1,8 @@
 
 var http=require('http');   
 var controller=function(request,response)
-		{    response.write("KYA NACHYA BHAI KYA BOLRI PUBLIC ");  
-			response.write(" CDAC KARRE BACCHE APNE VISHAL AUR RAHUL ");
+		{    response.write("KYA SOMYA BHAI KYA BOLRI PUBLIC ");  
+			response.write("PIMPLE KO BHEJNE KA KYA ILOVE U LIKH KE PUBLIC");
 			response.end();
 			};
 var server=http.createServer(controller);server.listen(7000);console.log("listening on port 7000");
